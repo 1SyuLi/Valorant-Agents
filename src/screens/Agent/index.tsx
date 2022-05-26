c759999a39078c9a5d55a6f832db71134b2dffa6
1SyuLi <ruan_goio10@hotmail.com>
2022-05-26 20:14:14 -0300

    Back Button in agent screen

--- src/screens/Agent/index.tsx
+++ src/screens/Agent/index.tsx
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import {
-    Container
+    Container,
+    BackButton,
 } from './styles';
 
 export function Agent() {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
+
     return (
         <Container>
             <StatusBar style='light' />
+
+            <TouchableOpacity onPress={handleGoBack}>
+                <BackButton name="chevron-left" />
+            </TouchableOpacity>
         </Container>
     );
 }
--- src/screens/Agent/styles.ts
+++ src/screens/Agent/styles.ts
@@ -1,6 +1,15 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
    flex: 1;
    background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const BackButton = styled(Feather)`
+    font-size: ${RFValue(32)}px;
+    color: ${({ theme }) => theme.colors.shape};
+    margin-top: 45px;
+    margin-left: 6px;
 `;
