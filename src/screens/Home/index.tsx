893215860b0a5668744d32c691f7446c989c0999
1SyuLi <ruan_goio10@hotmail.com>
2022-05-26 19:44:29 -0300

    Valorant Api - Agent Image

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 
 import logo from '../../assets/valorant.png';
+import { api } from '../../services/api';
 import { Card } from './Card';
 
 import {
@@ -12,7 +13,43 @@ import {
     Content,
 } from './styles';
 
+interface Agents {
+    id: string;
+    name: string;
+    image: string;
+}
+
 export function Home() {
+
+    const [agents, setAgents] = useState<Agents[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        async function loadAgents() {
+            await api.get("/agents", { params: { isPlayableCharacter: true } }).then(response => {
+                response.data.data.map(agent => {
+
+                    const formattedAgent = {
+                        id: agent.uuid,
+                        name: agent.displayName,
+                        image: agent.displayIcon,
+                    }
+
+                    setAgents(oldState => [...oldState, formattedAgent]);
+                    console.log(formattedAgent);
+                })
+            })
+        }
+
+        if (!load) {
+            loadAgents();
+            setLoad(true);
+        }
+        setLoading(false);
+    }, [])
+
+
     return (
         <Container>
             <StatusBar style='light' />
@@ -22,9 +59,17 @@ export function Home() {
                 <Title>Valorant Agents</Title>
             </Header>
 
-            <Content>
-                <Card image='' />
-            </Content>
+
+            {!loading &&
+                <>
+                    <Content>
+                        {agents.map(agent => <Card key={agent.id} image={agent.image} />)}
+                    </Content>
+                </>
+
+            }
+
+
         </Container>
     );
 }
